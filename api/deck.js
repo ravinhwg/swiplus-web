1c9b9434e30b7a8afed8d5987b1aee61a1d1b47a
Ravin <ravinhwg@gmail.com>
2021-06-24 11:43:31 +0530

    Change to prod.

--- api/deck.js
+++ api/deck.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/v1";
+const API_URL = "http://api.swiplus.com/v1";
 async function uploadDeck({ formdata, token }) {
   try {
     const response = await axios.post(`${API_URL}/decks`, formdata, {
--- api/search.js
+++ api/search.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/v1";
+const API_URL = "http://api.swiplus.com/v1";
 
 async function getSearchResults({ pageParam = 0, queryKey }) {
   const [, q] = queryKey;
