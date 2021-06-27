4497d2b67e66ed629e036deec50a96aafa1edf46
Ravin <ravinhwg@gmail.com>
2021-06-27 13:51:56 +0530

    ignore eslint during builds.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ module.exports = {
   images: {
     domains: ["picsum.photos", "storage.googleapis.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   env: {
     API_URL: process.env.API_URL,
   },
