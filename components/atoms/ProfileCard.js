4e676aec2ed0bfc40470b80727fbd04d1f805418
Ravin <ravinhwg@gmail.com>
2021-07-07 13:29:15 +0530

    Hotfix: profileCard filling.

--- components/atoms/ProfileCard.js
+++ components/atoms/ProfileCard.js
@@ -12,38 +12,42 @@ export default function ProfileCard({ user }) {
       {user ? (
         <Link href={`/${user.username}`}>
           <button type="button" className="focus:outline-none">
-            <div className="flex">
-              <div className="flex flex-1">
-                {user.profile_pic === null ? (
-                  <Image
-                    height="200"
-                    width="200"
-                    src="https://storage.googleapis.com/swiplusimages/profile_pics/default.jpeg"
-                    alt="profile-pic"
-                    className="rounded-full"
-                  />
-                ) : (
-                  <Image
-                    height="200"
-                    width="200"
-                    src={user.profile_pic}
-                    alt="profile-pic"
-                    className="rounded-full"
-                  />
-                )}
+            <div className="flex flex-col">
+              <div className="flex">
+                <div>
+                  {user.profile_pic === null ? (
+                    <Image
+                      height="50"
+                      width="50"
+                      src="https://storage.googleapis.com/swiplusimages/profile_pics/default.jpeg"
+                      alt="profile-pic"
+                      className="rounded-full"
+                    />
+                  ) : (
+                    <Image
+                      height="50"
+                      width="50"
+                      src={user.profile_pic}
+                      alt="profile-pic"
+                      className="rounded-full"
+                    />
+                  )}
+                </div>
+                <div className="ml-3">
+                  <p className=" text-gray-50 font-inter font-bold text-2xl text-left">
+                    {user.display_name.slice(0, 15)}
+                  </p>
+                  <p className=" text-gray-200 font-inter font-bold text-md text-left">
+                    @{user.username}
+                  </p>
+                </div>
               </div>
-              <div className="ml-3">
-                <p className=" text-gray-50 font-inter font-bold text-2xl text-left">
-                  {user.display_name}
-                </p>
-                <p className=" text-gray-200 font-inter font-bold text-md text-left">
-                  @{user.username}
+              <div className="mt-5">
+                <p className=" text-gray-500 font-inter text-md text-left">
+                  {user.bio?.substring(0, 60)}
                 </p>
               </div>
             </div>
-            <p className=" text-gray-500 font-inter text-md text-left">
-              {user.bio?.substring(0, 60)}
-            </p>
           </button>
         </Link>
       ) : (
